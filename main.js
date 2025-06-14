1c597bad3584e460769442123aaee7122f00aede
cooper-710 <cooprobinson7@gmail.com>
2025-06-14 10:41:33 -0400

    Update main.js

--- main.js
+++ main.js
@@ -786,23 +786,23 @@ document.getElementById("cameraSelect").addEventListener("change", (e) => {
       camera.lookAt(0, 2.5, 0);
       break;
     case "pitcher":
-      camera.position.set(0, 6.0, 0);  // slightly higher pitcher view
+      camera.position.set(0, 6.0, 2);  // slightly higher pitcher view
       camera.lookAt(0, 2.5, -60.5);
       break;
     case "rhh":
-      camera.position.set(1.5, 6.0, -63);  // higher right-handed hitter view
-      camera.lookAt(0, 6, 0);           // look at pitcher
+      camera.position.set(1.5, 6.0, -65);  // higher right-handed hitter view
+      camera.lookAt(0, 5, 0);           // look at pitcher
       break;
     case "lhh":
-      camera.position.set(-1.5, 6.0, -63); // higher left-handed hitter view
-      camera.lookAt(0, 6, 0);           // look at pitcher
+      camera.position.set(-1.5, 6.0, -65); // higher left-handed hitter view
+      camera.lookAt(0, 5, 0);           // look at pitcher
       break;
     case "1b":
-      camera.position.set(20, 5, -55);
+      camera.position.set(20, 5, -30);
       camera.lookAt(0, 5, -30);
       break;
     case "3b":
-      camera.position.set(-20, 5, -55);
+      camera.position.set(-20, 5, -30);
       camera.lookAt(0, 5, -30);
       break;
   }
